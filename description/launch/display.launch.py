f27fa185fc1c06299bf1dfd02cbc93b3c5d90955
Adrian Riadi <34883039+corvus-rex@users.noreply.github.com>
2025-05-16 22:29:53 +0700

    Update display.launch.py

--- description/launch/display.launch.py
+++ description/launch/display.launch.py
@@ -3,6 +3,7 @@ from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory  # ✅ added import
 import os
 
 def generate_launch_description():
